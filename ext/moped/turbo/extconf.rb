ea6bfbfa59ab13233a22a2ea7c7ff96a7b2bf297
Durran Jordan <durran@gmail.com>
2012-11-17 22:13:48 +0100

    Use moped/turbo in extconf

--- ext/moped/turbo/extconf.rb
+++ ext/moped/turbo/extconf.rb
@@ -1,3 +1,3 @@
 require "mkmf"
 $CFLAGS << " -Wall -g"
-create_makefile("turbo")
+create_makefile("moped/turbo")
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ require "moped"
 RSpec.configure do |config|
 
   config.before(:suite) do
-    Dir.chdir(Pathname(__FILE__).dirname.parent + "ext") do
-      `bundle exec ruby moped/turbo/extconf.rb`
+    Dir.chdir(Pathname(__FILE__).dirname.parent + "ext/moped/turbo") do
+      `bundle exec ruby extconf.rb`
       `make`
     end
     require "turbo"
